71e5ada17bee4e9e3eebcf641de5c640b3175f4c
Braden Katzman <bradenkatzman@gmail.com>
2018-01-26 10:24:11 -0500

    breaking up main driver into separate modules

--- src/TissueModeling_MainDriver.m
+++ src/TissueModeling_MainDriver.m
@@ -15,16 +15,20 @@ load_dataset_config.m
 
 
 % PART 1 - LOAD THE NUC DATA
-loadEmbryo.,
+loadEmbryo.m
+
+
+% PART 2 - call the respective modeling modules
+pharynx_modeling_driver.m
+
 
 % PART 3 - LOAD TISSUE DATA (tissue specific cells - names, time, diameter, name - at all timepoints)
-
 % nuc data format is 5xNxM
 % - 5 items represent x,y,z,diam,name
 % - N represents the maximum amount of cells present at a time point for the tissue 
 %   (in time points with less than this maximum, blank cells will be indicated by -1s)
 % - M represents the number of frames of cells to be modeled  
-pharynx_nuc_data = []
+
 %/
 % dl_muscle_nuc = []
 % dr_muscle_nuc = []
@@ -35,17 +39,4 @@ pharynx_nuc_data = []
 
 % PART 4 - SAVE THIS INFO TO FILE
 
-
-% ------------------------ THIS IS ONLY NECESSARY ONCE PER EMRBYO --------------------
-
-
-% PART 2 - INTERPOLATE TO LANDMARKS, EXPAND POINT CLOUD (OPTIONAL)
-
-% PART 3 - COMPUTE AN ALPHA SHAPE ON A POINT CLOUD
-
-% PART 4 - COMPUTE A VORONOI TESSELLATION ON A POINT CLOUD
-
-% PART 5 - 
-
-% PART 6 - INTERSECT THE ALPHA SHAPE WITH THE VORONOI TESSELLATION
 
